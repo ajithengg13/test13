63d07beae5aea0ddd706c8386f8030ce1280fbb7
Ubuntu <vmadmin@linuxkub.muzuqfnfe2gexm3hsy2y1nuy4a.cx.internal.cloudapp.net>
2021-06-18 05:48:51 +0000

    v1.0

---
+++ test01.tf
@@ -0,0 +1,14 @@
+provider "azurerm"{
+    features{}
+
+    
+subscription_id = "8c7735b0-f842-43cb-82d6-41cd5d6a5768"
+client_id       = "c720b6b3-6d39-4708-bd0a-d9d3f39a0544"
+client_secret   = "btQPb87QRNRJpJHdNl-0su9~--N3Xg6b77"
+tenant_id       = "ff4dcff7-11b2-42ca-ad06-19f7823eee27"
+}
+
+resource "azurerm_resource_group" "demo" {
+    name = "testaj1"
+    location = "westus"
+}
